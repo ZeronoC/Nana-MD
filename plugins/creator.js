35c83f5c939da459215bc30a9ecbc5829734abcc
Zeron Official <94586700+ZeronoC@users.noreply.github.com>
2022-01-21 08:01:21 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -2,12 +2,12 @@ const PhoneNumber = require('awesome-phonenumber')
 async function handler(m) {
                 let vcard = 'BEGIN:VCARD\n' // metadata of the contact card
                     + 'VERSION:3.0\n' 
-                    + 'N:;Adul Alhy;;;'
-                    + 'FN:Adul Alhy\n' // full name
-                    + 'ORG:Adul Alhy;\n' // the organization of the contact
-                    + 'TEL;type=CELL;type=VOICE;waid=6281390199407:+62 813-9019-9407\n' // WhatsApp ID + phone number
+                    + 'N:;iyyan p;;;'
+                    + 'FN:iyyan p\n' // full name
+                    + 'ORG:iyyan p;\n' // the organization of the contact
+                    + 'TEL;type=CELL;type=VOICE;waid=6289614412045:+62 896-1441-2045\n' // WhatsApp ID + phone number
                     + 'END:VCARD'
-                conn.sendMessage(m.chat, { contacts: { displayName: 'Adul Alhy', contacts: [{ vcard }] } }, { quoted: m })
+                conn.sendMessage(m.chat, { contacts: { displayName: 'iyyan', contacts: [{ vcard }] } }, { quoted: m })
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
